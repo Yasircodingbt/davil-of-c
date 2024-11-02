e6c532668a5e84a85e7ec585915185240df80109
yasircodingbt <yasircoding_bt@outlook.com>
2024-11-02 20:00:17 +0500

    local change

--- program.c
+++ program.c
@@ -1,2 +1,22 @@
 #include<stdio.h>
-#include
+#include<math.h>
+int mix(int a,int b);
+int main(){
+     int a,b;
+     printf(" enter a num : ");
+     scanf("%d",&a);
+     printf("enter a second num : ");
+     scanf("%d",&b); 
+int m= mix(a,b);
+     printf(" mixmum num is %d ", m);
+
+  return 0;
+} int mix(int a ,int b){
+     if (a>b)
+     {
+          return a;
+     }else{
+          return b;
+     }
+     
+}
