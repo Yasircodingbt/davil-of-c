d44c7c97dc2cd01ff28ea1391064e2e55c3ba0fa
yasircodingbt <yasircoding_bt@outlook.com>
2024-11-13 19:11:24 +0500

    first magic of recursion

--- recursion-1.c
+++ recursion-1.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 #include<math.h>
-int main(){
+void printhw(int count);
+int main(){  
+        
 
-
-
-
-
-
-
-
-
-
+printhw(5);
 
 return 0;
-
+ 
+}void printhw(int count){
+if(count ==0){  
+     return;
+}
+printf("hello word \n");
+printhw(count-1);
 }
